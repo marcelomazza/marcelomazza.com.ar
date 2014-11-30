21cb67f6699491c2e28852911aa75a780137d5fb
Marcelo Mazza <mnmazza@gmail.com>
2014-11-30 14:41:36 +0100

    refining the header colors and behavior

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -6,7 +6,6 @@ var portfolioUI = {
 	init: function() {
 
 		this.bindLocalScroll();
-		this.bindSwitchColors();
 		this.bindNavToggle();
 		this.bindOnScroll();
 
@@ -40,18 +39,6 @@ var portfolioUI = {
 
 		},
 
-	bindSwitchColors: function() {
-
-		$('.js-switch-colors').on('click', function(e) {
-
-			e.preventDefault();
-
-			$('body').toggleClass('switch');
-
-		});
-		
-	},
-
 	bindNavToggle: function() {
 
 		$('.js-nav-toggle').on('click', function(e) {
@@ -66,15 +53,74 @@ var portfolioUI = {
 
 	bindOnScroll: function() {
 
-		$(window).on('scroll', function() {
+		// $(window).on('scroll', function() {
 
-			console.log('scrolling');
-			$('.js-header-no-scroll').removeClass('js-header-no-scroll');
+		// 	console.log('scrolling');
+		// 	$('.js-header-no-scroll').removeClass('js-header-no-scroll');
 
-		});
+		// });
 
 	}
 
 };
 
 portfolioUI.init();
+
+
+// // Hide Header on on scroll down
+// var didScroll;
+// var lastScrollTop = 0;
+// var delta = 5;
+
+// $(window).scroll(function(event){
+//     didScroll = true;
+// });
+
+// setInterval(function() {
+//     if (didScroll) {
+//         hasScrolled();
+//         didScroll = false;
+//     }
+// }, 250);
+
+// setInterval(function() {
+//     if (!didScroll) {
+//     	hasNotScrolled();
+//     }
+// }, 5000);
+
+// function hasScrolled() {
+//     var st = $(this).scrollTop();
+
+//     // Make sure they scroll more than delta
+//     if (Math.abs(lastScrollTop - st) <= delta) {
+//         return;
+//     }
+    
+//     // If they scrolled down and are past the navbar, add class .nav-up.
+//     // This is necessary so you never see what is "behind" the navbar.
+//         // Scroll Down
+//         $('header').removeClass('nav-up').addClass('nav-down');
+//         // Scroll Up
+//         if(st === 0) {
+//             $('header').removeClass('nav-down').addClass('nav-up');
+//         }
+    
+//     lastScrollTop = st;
+// }
+
+// function hasNotScrolled() {
+
+// 	$('header').removeClass('nav-down').addClass('nav-up');
+
+// }
+
+// function hideNav() {
+
+// 	$(window).on('mousemove', function() {
+
+// 		$('header').removeClass('nav-up').addClass('nav-down');
+
+// 	});
+
+// }
