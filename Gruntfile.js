6a74a0b7157c09f9f6418d020b2fdadaf0009cfb
Marcelo Mazza <ux@marcelomazza.com.ar>
2016-06-05 10:40:14 -0300

    Tabs to spaces and reindent

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,61 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-	require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
-	grunt.initConfig({
-		assemble: {
-			project: {
-				options: {
-					partials: 'partials/*.hbs'
-				},
-				files: {
-					'index.html': ['pages/index.hbs']
-				}
-			}
-		},
-		autoprefixer: {
-		    multiple_files: {
-		      expand: true,
-		      flatten: true,
-		      src: 'css/*.css',
-		      dest: 'css/'
-		    }
-		},
-		responsive_images: {
-			myTask: {
-				options: {},
-				files: [{
-					expand: true,
-					src: ['**.{jpg,gif,png}'],
-					cwd: 'img/orig/',
-					dest: 'img/'
-				}]
-			}
-		},
-		sass: {
-			options: {
-				sourceMap: true,
-				outputStyle: 'compressed'
-			},
-			dist: {
-				files: {
-					'css/style.css': 'scss/style.scss'
-				}
-			}
-		},
-		watch: {
-			src: {
-				files: ['scss/*.scss'],
-				tasks: ['default']
-			}
-		}
-	});
+  grunt.initConfig({
+    assemble: {
+      project: {
+        options: {
+          partials: 'partials/*.hbs'
+        },
+        files: {
+          'index.html': ['pages/index.hbs']
+        }
+      }
+    },
+    autoprefixer: {
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src: 'css/*.css',
+        dest: 'css/'
+      }
+    },
+    responsive_images: {
+      myTask: {
+        options: {},
+        files: [{
+          expand: true,
+          src: ['**.{jpg,gif,png}'],
+          cwd: 'img/orig/',
+          dest: 'img/'
+        }]
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true,
+        outputStyle: 'compressed'
+      },
+      dist: {
+        files: {
+          'css/style.css': 'scss/style.scss'
+        }
+      }
+    },
+    watch: {
+      src: {
+        files: ['scss/*.scss'],
+        tasks: ['default']
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-responsive-images');
 
-	grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass']);
 
 };
